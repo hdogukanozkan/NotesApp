b308eb70c6cbea3e5468ded1c5aea08d924a0edd
Hanifi Dogukan Ozkan <hanifidogukanozkan@gmail.com>
2022-11-06 20:01:23 +0300

    Animation-cssFixed

--- src/component/NoteList.jsx
+++ src/component/NoteList.jsx
@@ -1,9 +1,12 @@
 import Note from "./Note";
 import EditNote from "./EditNote";
+import { useRef } from "react";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
-export default function noteList({ notes, handleAddNote, deleteNote }) {
+export default function NoteList({ notes, handleAddNote, deleteNote }) {
+  const [parent] = useAutoAnimate();
   return (
-    <div className="NoteList">
+    <div className="NoteList" ref={parent}>
       {notes.map((note) => (
         <Note note={note} key={note.id} deleteNote={deleteNote} />
       ))}
